611b2288225009f10d4675790246fb2aee224870
SnowCat <snowcat@vpro.ru>
2013-02-05 22:14:56 +0600

    UPDATE separate search block

--- _modules/_module.doc/_pages/template.doc_page_catalog.php
+++ _modules/_module.doc/_pages/template.doc_page_catalog.php
@@ -1,9 +1,5 @@
 <? function doc_page_catalog(&$db, &$menu, &$data){
 	$id = $db->id();
-	
-	$search = getValue('search');
-	if (!is_array($search)) $search = array();
-	if ($search) $search = array('prop' => $search);
 ?>
 <link rel="stylesheet" type="text/css" href="../../../_templates/baseStyle.css"/>
 {beginAdmin}
@@ -12,84 +8,8 @@
 {endAdminTop}
 
 <p>{{doc:read:menu=parent:$id;type:catalog}}</p>
-<?
-$s	= array();
-$sql= array();
-$s['parent'] 	= $id;
-$s['type']		= 'product';
-dataMerge($s, $search);
-doc_sql(&$sql, $s);
-?>
-<? if (beginCompile($data, $searchHash = "search_".hashData($sql))){ ?>
-<?
-$ids = array();
-$db->open($sql);
-while($db->next()) $ids[] = $db->id();
-$ids = makeIDS($ids);
-?>
-<? if ($prop = module("prop:get:$ids:Свойства товара")){ ?>
-<table width="100%" cellpadding="0" cellspacing="0" class="search property">
-<tr><td colspan="2" class="title">
-<big>Ваш выбор:</big>
-<?
-$bHasProp = false;
-foreach($search['prop'] as $name => $val){
-	if (!isset($prop[$name])) continue;
-	$bHasProp = true;
-	
-	$s		= $search;
-	unset($s['prop'][$name]);
-	$url	= getURL("page$id", makeQueryString($s['prop'], 'search'));
-	$val	= propFormat($val, $prop[$name]);
-?>
-<span><a href="{!$url}">{!$val}</a></span>
-<? } ?>
-<? if ($bHasProp){ ?>
-<a href="{{getURL:page$id}}" class="clear">очистить</a>
-<? } ?>
-</div>
-</td><tr>
-<?
-$ddb = module('doc');
-foreach($prop as $name => $val)
-{
-	if ($name[0] == ':') continue;
-	
-	$property = $val['property'];
-	if (!$property) continue;
-	if (isset($search['prop'][$name])) continue;
-	
-	$property = explode(', ', $property);
-	@$thisVal = $search['prop'][$name];
-?>
-<tr>
-    <th>{$name}:</th>
-    <td width="100%">
-<?
-foreach($property as $p)
-{
-	$class	= $thisVal == $p?' selected="selected"':'';
 
-	$s					= $search;
-	$s['parent']		= $id;
-	$s['prop'][$name]	= $p;
-
-	$sql= array();
-	doc_sql($sql, $s);
-	$ddb->open($sql);
-	$count	= $ddb->rows();
-
-	$nameFormat	= propFormat($p, $val);
-	$url		= getURL("page$id", makeQueryString($s['prop'], 'search'));
-?>
-<span><a href="{!$url}">{!$nameFormat}</a> ({$count})</span>
-<? }//	each prperty ?>
-	</td>
-</tr>
-<? }// each prop ?>
-</table>
-<? }// if prop ?>
-<? endCompile($data, $searchHash); }// compile ?>
+<? $search = module("doc:search:$id:Свойства товара", getValue('search')) ?>
 <div class="product list">
 <?	if ($search){ ?>
 <h2>Поиск по каталогу</h2>
---
+++ _modules/_module.doc/template.doc_search.php
@@ -0,0 +1,113 @@
+<?
+function doc_search($db, $val, $search)
+{
+	@list($id, $group) = explode(':', $val);
+	//	Откроем документ
+	$data	= $db->openID($id);
+	if (!$data) return;
+	
+	//	Проверим параметры поиска
+	if (!is_array($search)) $search = array();
+	if ($search) $search = array('prop' => $search);
+
+	$sql= array();
+	//	Подготовим базовый SQL запрос
+	$s	= array();
+	$s['parent'] 	= $id;
+	$s['type']		= 'product';
+	dataMerge($s, $search);
+	doc_sql(&$sql, $s);
+
+	//	Вычислим хеш значение, посмотрим кеш, если есть совпаления, то выведем результат и выйдем
+	if (!beginCompile($data, $searchHash = "search_".hashData($sql)))
+		return $search;
+
+	//////////////////
+	//	Созание поиска
+	
+	$ids = array();
+	$db->open($sql);
+	while($db->next()) $ids[] = $db->id();
+	$ids = makeIDS($ids);
+	
+	$prop = module("prop:get:$ids:$group");
+	if (!$prop){
+		endCompile($data, $searchHash);
+		return $search;
+	}
+	
+	///////////////////
+	//	Табличка поиска
+?>
+<table width="100%" cellpadding="0" cellspacing="0" class="search property">
+<tr><td colspan="2" class="title">
+<big>Ваш выбор:</big>
+<?
+//	Выведем уже имеющиеся в поиске варианты
+$bHasProp	= false;
+@$s1		= $search['prop'];
+while(@list($name, $val) = each($s1)){
+	//	Если в свойствах базы данных нет имени свойства,пропускаем
+	if (!isset($prop[$name])) continue;
+	$bHasProp = true;
+	
+	//	Соделаем ссылку поиска но без текущего элемента
+	$s		= $search;
+	unset($s['prop'][$name]);
+	$url	= getURL("page$id", makeQueryString($s['prop'], 'search'));
+	$val	= propFormat($val, $prop[$name]);
+	//	Показем значение
+?>
+<span><a href="{!$url}">{!$val}</a></span>
+<? } ?>
+<? if ($bHasProp){ ?>
+<a href="{{getURL:page$id}}" class="clear">очистить</a>
+<? } ?>
+</div>
+</td><tr>
+<?
+//	Выведем основные характеристики
+foreach($prop as $name => $val)
+{
+	if ($name[0] == ':') continue;
+	
+	$property = $val['property'];
+	if (!$property) continue;
+	if (isset($search['prop'][$name])) continue;
+	
+	$property = explode(', ', $property);
+	@$thisVal = $search['prop'][$name];
+?>
+<tr>
+    <th>{$name}:</th>
+    <td width="100%">
+<?
+foreach($property as $p)
+{
+	$class	= $thisVal == $p?' selected="selected"':'';
+
+	$s					= $search;
+	$s['parent']		= $id;
+	$s['prop'][$name]	= $p;
+
+	$sql= array();
+	doc_sql($sql, $s);
+	$db->fields = 'count(*) as cnt';
+	$db->open($sql);
+	$d		= $db->next();
+	!$count	= $d['cnt'];
+
+	$nameFormat	= propFormat($p, $val);
+	$url		= getURL("page$id", makeQueryString($s['prop'], 'search'));
+?>
+<span><a href="{!$url}">{!$nameFormat}</a> ({$count})</span>
+<? }//	each prperty ?>
+	</td>
+</tr>
+<? }// each prop ?>
+</table>
+<?
+	endCompile($data, $searchHash);
+	return $search;
+} ?>
+
