08dc42837c0162bdec219142fe462c63ffe34732
SnowCat <snowcat@vpro.ru>
2013-11-07 21:47:03 +0600

    UPDATE
    Reduce index.php file size, split compile modules functionally

--- index.php
+++ index.php
@@ -40,7 +40,6 @@ ob_start();
 globalInitialize();
 localInitialize();
 ob_end_clean();
-
 //////////////////////
 //	MAIN CODE
 //////////////////////
@@ -80,6 +79,7 @@ if (is_null($renderedPage))
 //	Завершить все выводы на экран
 //	Возможна постобработка страницы
 event('site.end',	$renderedPage);
+
 //	Обработчики GZIP и прочее
 event('site.close',	$renderedPage);
 //	Вывести в поток
@@ -614,7 +614,6 @@ function compileFiles($localCacheFolder)
 
 	$packages	= $ini[":packages"];
 	if (!is_array($packages))$packages = array();
-
 	modulesConfigure($localCacheFolder, $enable, $packages);
 	
 	//	При необходимости вывести сообщения от модулей в лог
@@ -624,23 +623,10 @@ function compileFiles($localCacheFolder)
 	module('message:trace:modules', trim(ob_get_clean()));
 	$time 		= round(getmicrotime() - $timeStart, 4);
 	m("message:trace", "$time Included ".localCompiledCode." file");
-	
-	//	Initialize pages and copy desing files
-	$localPages = array();
-	//	_modules
-	pagesInitialize(modulesBase,	$localPages, $enable);
-	//	_templates
-	pagesInitialize(templatesBase,	$localPages, $enable);
-	//	_packages checked for compile
-	foreach($packages as $path)	pagesInitialize($path,		$localPages, $enable);
-	//	sitepath/all files
-	pagesInitialize(localHostPath,	$localPages, $enable);
 
-	$bOK	= pageInitializeCopy($localCacheFolder.'/'.localSiteFiles, 		$localPages);
-	$bOK	&=	pageInitializeCompile($localCacheFolder,	$localPages);
-	if ($bOK)	setCacheValue('pages', $localPages);
-	else echo 'Error copy design files';
-	
+	//	Скомпилировать шаблоны, скопировать измененные файлы
+	event('config.prepare', $localCacheFolder);
+
 	$ini	= getCacheValue('ini');
 	event('config.end', $ini);
 	
@@ -673,11 +659,9 @@ function modulesConfigure($localCacheFolder, &$enable, &$packages)
 			readfile($modulePath);
 			echo "\r\n";
 		};
+		
 		$bOK = file_put_contents_safe($compiledPath, ob_get_clean());
-		$modulesPath	= $localCacheFolder.'/'.localSiteFiles;
-		$bOK&= pageInitializeCopy($modulesPath, $localModules);
-
-		if (!bOK){
+		if (!$bOK){
 			echo 'Error write compiled modules';
 			die;
 		};
@@ -710,122 +694,6 @@ function modulesInitialize($modulesPath, &$localModules, &$enable)
 	};
 }
 
-//	Поиск всех страниц и шаблонов
-function pagesInitialize($pagesPath, &$pages, &$enable)
-{
-	$module = basename($pagesPath);
-	if (isset($enable[$module])) return;
-
-	//	Поиск страниц сайта и шаблонов, запомниить пути для возможного копирования локальных файлов
-	$files	= getFiles($pagesPath, '^(page\.|phone\.page\.|tablet\.page\.|template\.).*\.(php|php3)$');
-	foreach($files as $name => $path){
-		$name = preg_replace('#\.[^.]*$#', '', $name);
-		$pages[$name] = $path;
-	}
-
-	$dirs = getDirs($pagesPath, '^_');
-	foreach($dirs as $pagePath){
-		//	Сканировать поддиректории
-		pagesInitialize($pagePath, $pages, $enable);
-	};
-}
-
-//	Копирование всех дизайнерских файлов из модуля в основной каталог сайта, за исключением системных файлов
-function pageInitializeCopy($rootFolder, $pages)
-{
-	$bOK = true;
-	makeDir($rootFolder);
-	foreach($pages as $pagePath)
-	{
-		$baseFolder	= dirname($pagePath);
-
-		//	Копирование файлов
-		$files 	= getFiles($baseFolder);
-		foreach($files as $name => $sourcePath)
-		{
-			//	Не копировать шиблоны страниц
-			if (preg_match('#^(page\.|.*\.page\.)#', $name)) continue;
-			//	Не копировать модули, конфиги, шаблоны
-			if (preg_match('#^(module_|config\.|template\.)#', $name)) continue;
-
-			$destPath = "$rootFolder/$name";
-			if ($sourcePath == $destPath) continue;
-			if (filemtime($sourcePath) == filemtime($destPath)) continue;
-
-			if (!copy($sourcePath, $destPath)){
-				$bOK = false;
-				continue;
-			}
-			touch($destPath, filemtime($sourcePath));
-		};
-		
-		//	Копирование папок
-		$dirs		= getDirs($baseFolder, '^[^_].+');
-		foreach($dirs as $name => $sourcePath)
-		{
-			if (is_int(strpos($sourcePath, images))) continue;
-			$bOK &= copyFolder($sourcePath, "$rootFolder/$name");
-		}
-	};
-	return $bOK;
-}
-
-//	Compile pages
-function pageInitializeCompile($localCacheFolder, &$pages)
-{
-	$templates			= array();
-	$comiledTemplates	= array();
-	$compiledTmpName	= "$localCacheFolder/".localCompilePath."/compiled.php3";
-	$compiledFileName	= localCacheFolder."/".localCompilePath."/compiled.php3";
-	$comiledFileTime	= NULL;
-
-	foreach($pages as $name => &$pagePath)
-	{
-		$fileName	= basename($pagePath);
-		if (strpos($fileName, ".php3") && preg_match('#^template\.#', $name))
-		{
-			$name					= preg_replace('#^template\.#', '', $name);
-			$templates[$name]		= $compiledFileName;
-			$comiledTemplates[$name]= $pagePath;
-			$comiledFileTime		= max($comiledFileTime, filemtime($pagePath));
-			$pagePath 				= $compiledFileName;
-			continue;
-		}
-
-		$compiledPagePath	= "$localCacheFolder/".localCompilePath."/$fileName";
-		if (filemtime($pagePath) != filemtime($compiledPagePath))
-		{
-			$compiledPage		= file_get_contents($pagePath);
-			event('page.compile', $compiledPage);
-			
-			if (!$compiledPage) continue;
-			if (!file_put_contents_safe($compiledPagePath, $compiledPage)) return false;
-			touch($compiledPagePath, filemtime($pagePath));
-		}
-		
-		$pagePath = localCacheFolder."/".localCompilePath."/$fileName";
-		if (preg_match('#^template\.#', $name)){
-			$name				= preg_replace('#^template\.#', '', $name);
-			$templates[$name]	= $pagePath;
-		}
-	}
-	
-	if ($comiledFileTime > filemtime($compiledTmpName))
-	{
-		$compiledTemplate	= '';
-		foreach($comiledTemplates as $name => &$pagePath)
-		{
-			$compiledPage		= file_get_contents($pagePath);
-/*			$compiledTemplate	.= "<? //	Template $name loaded from  $pagePath ?>\r\n";*/
-			$compiledTemplate	.=$compiledPage;
-		}
-		event('page.compile', $compiledTemplate);
-		file_put_contents_safe($compiledTmpName, $compiledTemplate);
-	}
-
-	setCacheValue('templates', $templates);
-	return true;
-}
 
 //	Получить локальный путь к папке с файлами сайта
 function getSitePath($siteURL)
