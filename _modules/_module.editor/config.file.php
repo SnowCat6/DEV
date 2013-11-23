c52853d3f965a2a12af1a57f7bed77612d2e2861
SnowCat <snowcat@vpro.ru>
2013-11-23 17:04:16 +0600

    ADD editor
    AJAX file upload and delete

--- _modules/_module.editor/config.file.php
+++ _modules/_module.editor/config.file.php
@@ -2,5 +2,8 @@
 addUrl('file_connector',  	'file:connector');
 addUrl('file_fconnector',	'file:connector:fck');
 addUrl('file_fconnector2',	'file:connector:fck2');
+
 addUrl('file_images',		'editor:images:ajax');
+addUrl('file_images_upload','editor:images:upload');
+addUrl('file_images_delete','editor:images:delete');
 ?>
--- _modules/_module.editor/template.editor_images.php
+++ _modules/_module.editor/template.editor_images.php
@@ -1,8 +1,42 @@
 ﻿<? function editor_images($val, $folder)
 {
-	m('script:editorImages');
+	if ($val == 'delete'){
+		setTemplate('');
+		$filePath	= getValue('fileImagesPath');
+		$filePath	= normalFilePath(images."/$filePath");
+		if (canEditFile($filePath)){
+			unlinkFile($filePath);
+		}else{
+			echo 'Error';
+		}
+		return;
+	}
+
+	if ($val == 'upload')
+	{
+		setTemplate('');
+		
+		$folder	= getValue('fileImagesPath');
+		$folder	= normalFilePath(images."/$folder");
+
+		$file		= $_FILES['imageFieldUpload'];
+		$fileName	= makeFileName($file['name']);
+		$filePath	= "$folder/$fileName";
+		
+		if (strpos($folder, '/Title') > 0) delTree($folder);
+		makeDir($folder);
+		unlinkFile($filePath);
+		if (move_uploaded_file($file['tmp_name'], $filePath)){
+			fileMode($filePath);
+			echo 'OK';
+		}else{
+			echo 'Error';
+		}
+		return;
+	}
+	
 	$url	= getValue('fileImagesPath');
-	if ($url && $val=='ajax'){
+	if ($val=='ajax'){
 		setTemplate('');
 		$folder	= $url;
 		if (!is_array($folder)) $folder= array($folder);
@@ -11,9 +45,10 @@
 		if (!is_array($folder)) $folder= array($folder);
 	}
 	
+	m('script:editorImages');
 	
-	$f	= $folder;
-	foreach($f as &$p2) $p2 = str_replace(globalRootURL.'/'.images.'/',	'', globalRootURL."/$p2");;
+	$f	= array();
+	foreach($folder as $name => &$p2) $f[$name] = str_replace(globalRootURL.'/'.images.'/',	'', globalRootURL."/$p2");
 	$url= makeQueryString($f, 'fileImagesPath');
 ?>
 <div class="editorImages">
@@ -23,7 +58,6 @@
 <div class="editorImageHolder shadow">
 <table cellpadding="0" cellspacing="0" width="100%">
 <?
-
 $name	= '';
 foreach($folder as $p)
 {
@@ -31,20 +65,28 @@ foreach($folder as $p)
 	
 	$name	= explode('/', $p);
 	$name	= htmlspecialchars(end($name));
-	echo "<tr><th colspan=\"2\">$name</th></tr>";
-	if (!$files){ ?>
+	$p3		= str_replace(globalRootURL.'/'.images.'/',	'', globalRootURL."/$p");
+?>
+	<tr>
+    <th colspan="2">{$name}
+    <span class="editroImageUpload" rel="{$p3}">+</span></th>
+    </tr>
+<?
+	if (!$files){
+?>
    <tr><td colspan="2" class="noImage">Нет изображений</td></tr>
-    <? }
+<? }
     
 	foreach($files as $name => &$path)
 	{
 		list($w, $h) = getimagesize($path);
 		if (!$w || !$h) continue;
 		$size	= "$w x $h";		
+		$p		= str_replace(globalRootURL.'/'.images.'/',	'', globalRootURL."/$path");
 ?>
 <tr>
-    <td><a href="/{$path}" target="_blank">{$name}</a></td>
-    <td class="size"><a href="/{$path}" target="_blank">{$size}</a></td>
+    <td class="image"><a href="/{$path}" target="_blank">{$name}</a></td>
+    <td class="size"><a href="#" rel="{$p}"><span>{$size}</span></a></td>
 </tr>
 <? } ?>
 <? } ?>
@@ -72,7 +114,8 @@ foreach($folder as $p)
 }
 .editorImages .editorImageHolder th{
 	background:#444;
-	padding:2px 5px;
+	padding:0 0 0 5px;
+	font-size:20px;
 }
 .editorImages .editorImageHolder a{
 	padding:5px 10px;
@@ -94,9 +137,23 @@ foreach($folder as $p)
 .editorImages:hover .editorImageHolder{
 	display:block;
 }
-.editorImages .editorImageHolder .size{
+.editorImageHolder .size{
 	text-align:right;
 	padding-right:20px;
+}
+.editorImageHolder .size a:hover{
+	background:red;
+}
+.editorImageHolder tr:hover .size a:hover:after{
+	content: "удалить";
+	color:white;
+}
+.editorImageHolder tr:hover .size a span{
+	display:none;
+}
+.editorImageHolder tr:hover .size a:after{
+	content: "вставить";
+	color:white;
 }
 .editorImageHolder .noImage{
 	padding:5px 10px;
@@ -116,11 +173,31 @@ foreach($folder as $p)
 	background:green !important;
 	cursor:wait;
 }
+.editroImageUpload{
+	float:right;
+	display:block;
+	width:26px;
+	text-align:center;
+	cursor:pointer;
+	padding:0;
+}
+.imageUploadForm{
+	display:block;
+	position:absolute;
+	top: 0; left: 0;
+	width:100%;	height:100%;
+}
+.imageFieldUpload{
+	width:100%; height:100%;
+	opacity: 0; filter:alpha(opacity: 0);
+}
 </style>
+<iframe name="imageUploadFrame" id="imageUploadFrame" style="display:none"></iframe>
 <script>
 $(function(){
-	$(document).on("jqReady ready", function(){
-		$(".editorImageHolder a").click(function(){
+	$(document).on("jqReady ready", function()
+	{
+		$(".editorImageHolder .image a").click(function(){
 			
 			var size = $(this).parent().parent().find(".size").text().split(" x ");
 			var html = '<img src="' + $(this).attr("href") + '"' + 'width="' + size[0] + '"' + 'height="' + size[1] + '"' + '/>';
@@ -132,6 +209,14 @@ $(function(){
 			}
 			return false;
 		});
+		
+		$(".editorImageHolder .size a").click(function(){
+			$(this).load('{{url:file_images_delete}}?fileImagesPath=' + $(this).attr("rel"), function(){
+				$(".editorImageReload").click();
+			});
+			return false;
+		});
+		
 		$(".editorImageReload").click(function(){
 			var r = $(this).parent();
 			r.html('<div class="editorImageReload reload"><span />Обновление...');
@@ -141,8 +226,22 @@ $(function(){
 			});
 			return false;
 		});
+		
+		$(".editroImageUpload").each(function()
+		{
+			$(this).css({"position": "relative"});
+			var form = $('<form action="{{url:file_images_upload}}" class="imageUploadForm" method="post" enctype="multipart/form-data" target="imageUploadFrame"></form>');
+			$('<input type="hidden" name="fileImagesPath" />').val($(this).attr("rel")).appendTo(form);
+			$('<input type="file" class="imageFieldUpload" name="imageFieldUpload" />').appendTo(form);
+			form.appendTo($(this)).change(function(){
+				$(this).submit();
+			});
+		});
 	});
 	
+	$("#imageUploadFrame").load(function(){
+		$(".editorImageReload").click();
+	});
 });
 </script>
 <? } ?>
