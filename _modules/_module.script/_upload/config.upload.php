3eebe31d45a8c9213a745ccbbd2c8c014fb35ae3
SnowCat <snowcat@vpro.ru>
2013-12-17 22:04:56 +0600

    FIX upload

--- _modules/_module.script/_upload/config.upload.php
+++ _modules/_module.script/_upload/config.upload.php
@@ -1,5 +1,5 @@
 ﻿<?
-addUrl('file_images_upload/(.+)',	'fileUpload:upload');
-addUrl('file_images_delete/(.+)',	'fileUpload:delete');
+addUrl('file_images_upload',		'fileUpload:upload');
+addUrl('file_images_delete',		'fileUpload:delete');
 addUrl('file_images_comment/(.+)',	'file:comment');
 ?>
