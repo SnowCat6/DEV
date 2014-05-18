251a6759aa95d6f1b1c72e09105382d9d0383db6
SnowCat <snowcat@vpro.ru>
2014-05-18 15:58:06 +0600

    UPDATE SQL tool

--- _modules/_module.admin/_settings/template.admin_SQLquery.php
+++ _modules/_module.admin/_settings/template.admin_SQLquery.php
@@ -1,44 +1,66 @@
 <? function admin_SQLquery(&$val)
 {
 	if (!hasAccessRole('developer')) return;
-//	setTemplate('');
 	$LogSQLquery	= getValue('LogSQLquery');
 ?>
-{{page:title=Выполнить код SQL}}
-{{script:ajaxForm}}
-<form action="{{url:admin_SQLquery}}" method="POST" class="ajaxForm ajaxReload">
-<h2 style="margin:0">SQL запрос: <input type="submit" value="RUN" class="button" /></h2>
-<textarea name="LogSQLquery" class="input w100" rows="5" style="font-size:11px">{$LogSQLquery}</textarea>
-<? if ($LogSQLquery){ ?>
+
 <?
 $db		= new dbRow();
-$time	= getmicrotime();
-$db->exec($LogSQLquery);
-$time	= round(getmicrotime() - $time, 5);
-$error	= $db->error();
+if ($LogSQLquery){
+	$time	= getmicrotime();
+	$db->exec($LogSQLquery);
+	$time	= round(getmicrotime() - $time, 5);
+	$error	= $db->error();
+}else{
+	$time	= '-';
+}
 ?>
-<h2>Результат: {$time} сек.</h2>
+
+{{page:title=Выполнить код SQL}}
+{{script:ajaxForm}}
+{{ajax:template=ajax_edit}}
+{{script:jq_ui}}
+<form action="{{url:admin_SQLquery}}" method="POST" class="ajaxForm ajaxReload">
+<div id="SQLtabs" class="ui-tabs ui-widget ui-widget-content ui-corner-all">
+<ul class="ui-tabs-nav ui-helper-reset ui-helper-clearfix ui-widget-header ui-corner-all">
+    <li class="ui-state-default ui-corner-top"><a href="#SQLquery">SQL запрос</a></li>
+    <li class="ui-state-default ui-corner-top"><a href="#SQLresult">Результат  {$time} сек.</a></li>
+	<li style="float:right"><input name="docSave" type="submit" value="Выполнить" class="ui-button ui-widget ui-state-default ui-corner-all" /></li>
+</ul>
+
+<div id="SQLquery">
+<textarea name="LogSQLquery" class="input w100" rows="15">{$LogSQLquery}</textarea>
+</div>
+
+<div id="SQLresult">
 <? messageBox($error) ?>
 <?
 $header	= false;
 while($data = $db->dbResult()){ ?>
 <? if (!$header){ $header=true; ?>
-<table class="table" cellpadding="0" cellspacing="0">
+    <table class="table" cellpadding="0" cellspacing="0">
+    <tr>
+    <? foreach($data as $name=>$val){ ?>
+        <th>{$name}</th>
+    <? } ?>
+    </tr>
+<? } ?>
 <tr>
 <? foreach($data as $name=>$val){ ?>
-<th>{$name}</th>
+    <td valign="top">{$val}</td>
 <? } ?>
-</tr>
-<? } ?>
-<tr>
-<? foreach($data as $name=>$val){ ?>
-<td valign="top">{$val}</td>
-<? } ?>
-</tr>
+    </tr>
 <? } ?>
 <? if ($header){ ?>
-</table>
+    </table>
 <? } ?>
-<? } ?>
+</div>
+
+</div>
 </form>
+<script>
+$(function(){
+	$("#SQLtabs").tabs();
+});
+</script>
 <? } ?>
