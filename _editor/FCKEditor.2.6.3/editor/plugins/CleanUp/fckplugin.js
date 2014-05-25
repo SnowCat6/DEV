e15ee0b9fd3a9299d14340b8207f1c63e723c0c3
SnowCat <snowcat@vpro.ru>
2014-05-25 12:01:53 +0600

    REMOVE FCKEditor 2.x

--- _editor/FCKEditor.2.6.3/editor/plugins/CleanUp/fckplugin.js
+++
@@ -1,5 +0,0 @@
-﻿
-FCKCommands.RegisterCommand( 'CleanUp', new FCKDialogCommand( 'CleanUp', FCKLang.CleanUpDlgTitle, FCKPlugins.Items['CleanUp'].Path + 'CleanUp.html',540,440));
-var oCleanUpItem = new FCKToolbarButton( 'CleanUp', FCKLang.CleanUpDlgTitle ) ;
-oCleanUpItem.IconPath = FCKPlugins.Items['CleanUp'].Path + 'CleanUp.gif' ;
-FCKToolbarItems.RegisterItem( 'CleanUp', oCleanUpItem ) ;
