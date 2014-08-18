d9c34e3ad22fd603cc29368bc8ef093fed27ebf3
SnowCat <snowcat@vpro.ru>
2014-08-18 13:47:33 +0600

    UPDATE htaccess generator

--- _modules/_core/_init/template.module_htaccess.php
+++ _modules/_core/_init/template.module_htaccess.php
@@ -7,28 +7,27 @@ function htaccessMake()
 	$globalRootURL	= globalRootURL;
 	$ctx = $ctxNow	= file_get_contents('.htaccess');
 	$sitesRules		= '';
-	//	Remove old .htaccess code
-	$ctx	= preg_replace("/# <= index.*# => index/s", '', $ctx);
 	
 	$ini	= getGlobalCacheValue('ini');
 	$sites	= getSiteRules();
 	foreach($sites as $rule => $host){
-		$sitesRules	.= "# $host\r\n";
 		htaccessMakeHost($rule, $host, $sitesRules, $ctx);
 	}
 	
 	$sitesRules	= "\r\n".
 	"# <= DEVCMS\r\n".
+	
 	"AddDefaultCharset UTF-8\r\n".
 	"ErrorDocument 404 /pageNotFound404\r\n".
 	"RewriteEngine On\r\n".
 	"RewriteRule \.htm$|pageNotFound404	$globalRootURL/index.php [NC,L]\r\n".
 	"$sitesRules\r\n".
+
 	"# => DEVCMS\r\n";
 
 	if (preg_match('/# <= DEVCMS.*# => DEVCMS/s', $ctx)){
 		$sitesRules	= str_replace("$", "\\$", $sitesRules);
-		$ctx		= preg_replace('/\s*# <= DEVCMS.*# => DEVCMS\s*/s', $sitesRules, $ctx);
+		$ctx		= preg_replace('/(\s*# <= DEVCMS.* #=> DEVCMS\s*)/s', $sitesRules, $ctx);
 	}else{
 		$ctx	.= $sitesRules;
 	}
@@ -38,32 +37,33 @@ function htaccessMake()
 }
 function htaccessMakeHost($hostRule, $hostName, &$ctx, &$htaccess)
 {
-	$safeName	= md5($hostRule);
-	//	Remove old .htaccess code
-	$htaccess	= preg_replace("/# <= $safeName.*# => $safeName/s", '', $htaccess);
-	
 	if (strncmp('http://', strtolower($hostName), 7) == 0){
 		$ctx	.=
+			"# $hostName\r\n".
 			"RewriteCond %{HTTP_HOST} $hostRule\r\n".
 			"RewriteRule .*	$hostName	[R=301,L]";
 	}else{
-		//	Initialize image path
-		$ini 			= readIniFile("_sites/$hostName/_modules/config.ini");
-		$localImagePath = $ini[':images'];
-		if (!$localImagePath) $localImagePath = 'images';
-		$localImagePath = trim($localImagePath, '/');
-		
-		$globalRootURL = globalRootURL;
-		$globalRootPath= globalRootPath;
+		//	Папка с разположением сайта
+		$localSiteFolder= sitesBase."/$hostName";
+		//	Базовый адрес сайта типа "/"
+		$globalRootURL	= globalRootURL;
+		//	Адрес кешированных файлов сайта
+		$localCacheURL	= "$globalRootURL/".globalCacheFolder."/$hostName/".localSiteFiles;
+		//	Адрес файлов сайта
+		$localSiteURL	= "$globalRootURL/$localSiteFolder";
 		
 		$ctx	.= 
+			"\r\n".
+			"# $hostName\r\n".
 			"RewriteCond %{HTTP_HOST} $hostRule\r\n".
-			"RewriteRule ^($localImagePath/.+)	$globalRootURL/_sites/$hostName/$1 [L]\r\n".
+			"RewriteCond %{REQUEST_URI} !^/_|\.php$\r\n".
+			"RewriteCond %{DOCUMENT_ROOT}$localSiteFolder%{REQUEST_URI} -f\r\n".
+			"RewriteRule (.*)	$localSiteURL/$1 [L]\r\n".
 		
 			"RewriteCond %{HTTP_HOST} $hostRule\r\n".
-			"RewriteCond %{REQUEST_FILENAME} !/_|\.php$\r\n".
-			"RewriteRule (.+)	_cache/$hostName/siteFiles/$1 [L]\r\n".
-			'';
+			"RewriteCond %{REQUEST_URI} !^/_|\.php$\r\n".
+			"RewriteRule (.*)	$localCacheURL/$1 [L]\r\n".
+			"";
 	}
 }
 ?>
