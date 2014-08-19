8a64f3ea523500262bdb0297c223b3c537108b09
SnowCat <snowcat@vpro.ru>
2014-08-19 15:34:54 +0600

    UPDATE htaccess generator
    Add security access for system folders

--- _modules/_core/_init/template.module_htaccess.php
+++ _modules/_core/_init/template.module_htaccess.php
@@ -5,6 +5,12 @@ function module_htaccess(){
 function htaccessMake()
 {
 	$globalRootURL	= globalRootURL;
+	$modulesFolder	= modulesBase;
+	$templatesFolder= templatesBase;
+	$cacheFolder	= globalCacheFolder;
+	$sitesFolder	= sitesBase;
+	$sitesCache		= localSiteFiles;
+	
 	$ctx = $ctxNow	= file_get_contents('.htaccess');
 	$sitesRules		= '';
 	
@@ -18,10 +24,24 @@ function htaccessMake()
 	"# <= DEVCMS\r\n".
 	
 	"AddDefaultCharset UTF-8\r\n".
-	"ErrorDocument 404 /pageNotFound404\r\n".
+	"ErrorDocument 404 $globalRootURL/index.php?URL=pageNotFound404\r\n".
+	"\r\n".
 	"RewriteEngine On\r\n".
-	"RewriteRule \.htm$|pageNotFound404	$globalRootURL/index.php [NC,L]\r\n".
+	"RewriteBase  /\r\n".
+	"# Disable rewrite loop\r\n".
+	"RewriteRule ^index	- [L]\r\n".
+	"# HTML pages handle\r\n".
+	"RewriteRule \.htm$	$globalRootURL/index.php [L]\r\n".
+	"# Allow redirected files access\r\n".
+	"RewriteRule ^($cacheFolder)/[^/]+/$sitesCache/	- [L]\r\n".
+	"RewriteRule ^($sitesFolder)/[^/]+/	- [L]\r\n".
+	"# Disable system folders access\r\n".
+	"RewriteRule ^($cacheFolder|$sitesFolder|$modulesFolder|$templatesFolder)	$globalRootURL/index.php [L]\r\n".
 	"$sitesRules\r\n".
+	"# Allow folders access\r\n".
+	"RewriteRule /	- [L]\r\n".
+	"# Disable all uncached links\r\n".
+	"RewriteRule .*	$globalRootURL/index.php [L]\r\n".
 
 	"# => DEVCMS\r\n";
 	
@@ -43,27 +63,26 @@ function htaccessMakeHost($hostRule, $hostName, &$ctx, &$htaccess)
 			"RewriteCond %{HTTP_HOST} $hostRule\r\n".
 			"RewriteRule .*	$hostName	[R=301,L]";
 	}else{
+		//	Физический к корню сайта
 		$globalRootPath	= globalRootPath;
-		//	Папка с разположением сайта
-		$localSiteFolder= sitesBase."/$hostName";
 		//	Базовый адрес сайта типа "/"
 		$globalRootURL	= globalRootURL;
+		
+		//	Папка с разположением сайта
+		$localSiteFolder= sitesBase."/$hostName";
 		//	Адрес кешированных файлов сайта
-		$localCacheURL	= "$globalRootURL/".globalCacheFolder."/$hostName/".localSiteFiles;
-		//	Адрес файлов сайта
-		$localSiteURL	= "$globalRootURL/$localSiteFolder";
+		$localCacheFolder= globalCacheFolder."/$hostName/".localSiteFiles;
 		
 		$ctx	.= 
 			"\r\n".
 			"# $hostName\r\n".
 			"RewriteCond %{HTTP_HOST} $hostRule\r\n".
-			"RewriteCond %{REQUEST_URI} !^/_|\.php$\r\n".
-			"RewriteCond $globalRootPath/$localSiteFolder/$"."0 -f\r\n".
-			"RewriteRule (.+)	$localSiteURL/$1 [L]\r\n".
+			"RewriteCond $globalRootPath/$localSiteFolder/$0 -f\r\n".
+			"RewriteRule .*	$globalRootURL/$localSiteFolder/$0 [L]\r\n".
 		
 			"RewriteCond %{HTTP_HOST} $hostRule\r\n".
-			"RewriteCond %{REQUEST_URI} !/_|\.php$\r\n".
-			"RewriteRule (.+)	$localCacheURL/$1 [L]\r\n".
+			"RewriteCond $globalRootPath/$localCacheFolder/$0 -f\r\n".
+			"RewriteRule .*	$globalRootURL/$localCacheFolder/$0 [L]\r\n".
 			"";
 	}
 }
