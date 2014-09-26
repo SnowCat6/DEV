87292f0a6a7aad3fa4bd9a0c5171561d4dbc8a9b
SnowCat <snowcat@vpro.ru>
2014-09-26 13:33:39 +0600

    FIX contentFX

--- _packages/_contentFX/script/contentFX.js
+++ _packages/_contentFX/script/contentFX.js
@@ -1,36 +1,45 @@
 // JavaScript Document
 
-$(function(){
-	$(".contentFX.image").wrapInner('<div class="content"></div>');
-	$(window).scroll(function(){
-		$(".contentFX.image").each(function()
-		{
-			var pageHeight = $(document).height();
-			var windowTop = $(window).scrollTop();
-			var windowHeight = $(window).height();
-			
-			var elmTop = $(this).offset().top;
-			var elmHeight = $(this).height();
-			
-			var percentTop = (elmTop - windowTop) / windowHeight;
-			var percentBottom = (elmTop + elmHeight - windowTop) / windowHeight;
-			
-			percentTop = Math.min(1, percentTop);
-			percentTop = Math.max(0, percentTop);
-
-			percentBottom = Math.min(1, percentBottom);
-			percentBottom = Math.max(0, percentBottom);
-			
-			var percent = 1-(percentTop + percentBottom)/2;
-			console.log(percent);
-			
-			var elmInner = $(this).find("> div");
-			var topOffset = Math.round((elmInner.height() - elmHeight) * percent);
-			
-			topOffset = Math.max(0, topOffset);
-			topOffset = Math.min(elmInner.height() - elmHeight, topOffset);
-			
-			elmInner.css({top: -topOffset});
-		});
-	});
+$(function()
+{
+	$(".contentFX.image")
+		.wrapInner('<div class="content"></div>');
+		
+	$(window)
+		.scroll(fnAutoscroll)
+		.resize(fnAutoscroll);
+		
+	fnAutoscroll();
 });
+function fnAutoscroll()
+{
+	$(".contentFX.image").each(function()
+	{
+		var pageHeight = $(document).height();
+		var windowTop = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		
+		var elmTop = $(this).position().top;
+		var elmHeight = $(this).height();
+		
+		var percentTop = (elmTop - windowTop) / windowHeight;
+		var percentBottom = (elmTop + elmHeight - windowTop) / windowHeight;
+		
+		percentTop = Math.min(1, percentTop);
+		percentTop = Math.max(0, percentTop);
+
+		percentBottom = Math.min(1, percentBottom);
+		percentBottom = Math.max(0, percentBottom);
+		
+		var percent = (percentTop + percentBottom)/2;
+//			console.log(percent);
+		
+		var elmInner = $(this).find("> div");
+		var topOffset = Math.round((elmInner.height()) * percent);
+
+		topOffset = Math.max(0, topOffset);
+		topOffset = Math.min(elmInner.height() - elmHeight, topOffset);
+		
+		elmInner.css({top: -topOffset});
+	});
+};
