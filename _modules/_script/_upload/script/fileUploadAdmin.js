e308bb3b0d830c73f2aa69e16f579952cb05e9b7
SnowCat <snowcat@vpro.ru>
2014-10-09 22:18:59 +0600

    UPDATE standalone site file browser

--- _modules/_script/_upload/script/fileUploadAdmin.js
+++ _modules/_script/_upload/script/fileUploadAdmin.js
@@ -12,63 +12,31 @@ $(function(){
 		{
 			try{
 				var cfg = $.parseJSON($(this).attr("rel"));
-				var folder = cfg['uploadFolder'];
-				var clip = folder.split('/');
-				clip = clip.splice(0, clip.length-1)
-				folder = clip.join('/');
+				var folder = _baseDir(cfg['uploadFolder']);
 			}catch(e){
 				return false;
 			}
 			
-			var ui = $("<div class='ajaxLoading'></div>")
+			var connector  = editorBaseFinder + "?Connector=file_fconnector2/" + folder + '.htm';
+			
+			var html = '<h1>' +  folder +'</h1>';
+			html += '<a href="#" class="ajaxClose">X</a>';
+			html += "<iframe src='" + connector + "'></iframe>";
+			var ui = $("<div class='fileUploadAdmin'>" + html + "</div>")
 			.overlay();
 
-			$.ajax("file_images_get.htm?fileImagesPath=" + folder)
-				.done(function(data)
-			{
-				$(".ajaxLoading").removeClass("ajaxLoading");
-				
-				try{
-					var responce = $.parseJSON(data);
-				}catch(e){ return; };
-				
-				_create(ui, responce, folder);
+			$(".ajaxClose").click(function(){
+				$().overlay("close");
+				return false;
 			});
 			
 			return false;
 		});
 		
-		function _create(ui, files, folder)
-		{
-			var html = '<h1>' +  folder +'</h1>';
-			html += '<a href="#" class="ajaxClose">X</a>';
-			
-			html += '<div class="content">';
-			html += '<form action="file_images_delete.htm">';
-			for(var file in files)
-			{
-				var f = files[file];
-				html += '<h2>' + file + '</h2>';
-				for(var f2 in f){
-					var f3 = folder + '/' + file + '/' + f2;
-					html += '<div><label><input type="checkbox" name="delete[]" value="' + f3 + '" />' + f2 + '</label></div>';
-				}
-			};
-			
-			html += '<p><input type="submit" value="Удалить" class="button"></p>';
-			html += '</form>';
-			html += '</div>';
-			
-			ui.addClass('fileUploadAdmin')
-			.html(html);
-			
-			$(".ajaxClose").click(function(){
-				$().overlay("close");
-				return false;
-			});
-			ui.find("form").ajaxForm(function(){
-				document.location.reload();
-			});
+		function _baseDir(path){
+			var clip = path.split('/');
+			clip = clip.splice(0, clip.length-1)
+			return clip.join('/');
 		}
 	}
 })( jQuery );
--- _modules/_script/_upload/template.script_fileUploadAdmin.php
+++ _modules/_script/_upload/template.script_fileUploadAdmin.php
@@ -2,6 +2,8 @@
 	m('script:jq');
 	m('script:overlay');
 	m('script:ajaxForm');
+
+	m('script:editorFCK4finder');
 	
 	m('fileLoad', 	'css/fileUploadAdmin.css');
 	m('scriptLoad', 'script/fileUploadAdmin.js');
