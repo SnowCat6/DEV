4c35fd8a85ead8d7c4eeff2ff57df0349fd5b53e
SnowCat <snowcat@vpro.ru>
2014-10-20 15:10:18 +0600

    UPDATE fullPageCache admin

--- _packages/_fullpageCache/template.admin_panel_fullpageCache.php
+++ _packages/_fullpageCache/template.admin_panel_fullpageCache.php
@@ -15,34 +15,62 @@ function admin_panel_fullpageCache(&$val)
 	if (!is_array($pages))	$pages = array();
 	if ($pages[$thisPage])	unset($pages[$thisPage]);
 ?>
+{{script:adminTabs}}
 <form method="post" action="{{url:#}}">
+<div class="adminTabs ui-tabs ui-widget ui-widget-content ui-corner-all">
+<ul class="ui-tabs-nav ui-helper-reset ui-helper-clearfix ui-widget-header ui-corner-all">
+    <li class="ui-corner-top"><a href="#fullPageCacheThis">Текущая страница</a></li>
+    <li class="ui-corner-top"><a href="#fullPageCacheNo">Не учитывая параметров</a></li>
+    <li class="ui-corner-top"><a href="#fullPageCacheYes">С учетом параметров</a></li>
+    <li style="float:right"><input name="docSave" type="submit" value="Сохранить" class="ui-button ui-widget ui-state-default ui-corner-all" /></li>
+</ul>
+
+<div id="fullPageCacheThis">
 <p>
-    <input  type="submit" value="Сохранить" class="button" />
+<label>
+    <input type="radio" name="fullpageCache[{$thisPage}]" value="" title="Не кешировать" />
+    Не кешировать
+</label>
 </p>
+
 <p>
-    <label><input type="radio" name="fullpageCache[{$thisPage}]" value="" /> - </label>
+<label>
     <input type="radio" name="fullpageCache[{$thisPage}]" value="noCheck" {checked:$ini[:fullpageCache][$thisPage]=='noCheck'} title="Кешировать без проверки параметров" />
-    <label>
-        <input type="radio" name="fullpageCache[{$thisPage}]" value="full" {checked:$ini[:fullpageCache][$thisPage]=='full'} title="Кешировать с проверкой параметров" />
-        Кешировать эту страницу <a href="{$thisPage}">{$thisPage}</a>
-<? if ($ini[':fullpageCache'][$thisPage]=='noCheck'){ ?>
-статическое кеширование, параметры не учитываются
-<? } ?>
-    </label>
+    Кешировать эту страницу не учитывая параметров
+</label>
 </p>
-<? foreach($pages as $thisPage=>$type){ ?>
-<div>
-    <label><input type="radio" name="fullpageCache[{$thisPage}]" value="" /> - </label>
-    <input type="hidden" name="fullpageCache[{$thisPage}]" value="" />
-    <input type="radio" name="fullpageCache[{$thisPage}]" value="noCheck" {checked:$ini[:fullpageCache][$thisPage]=='noCheck'} title="Кешировать без проверки параметров" />
-    <label>
-        <input type="radio" name="fullpageCache[{$thisPage}]" value="full" {checked:$ini[:fullpageCache][$thisPage]=='full'} title="Кешировать с проверкой параметров" />
-        Кешировать эту страницу <a href="{$thisPage}">{$thisPage}</a> 
-    </label>
-<? if ($ini[':fullpageCache'][$thisPage]=='noCheck'){ ?>
-статическое кеширование, параметры не учитываются
-<? } ?>
+
+<p>
+<label>
+    <input type="radio" name="fullpageCache[{$thisPage}]" value="full" {checked:$ini[:fullpageCache][$thisPage]=='full'} title="Кешировать с проверкой параметров" />
+    Кешировать эту страницу с учетом параметров
+</label>
+</p>
 </div>
-<? } ?>
+
+<div id="fullPageCacheNo">
+<? showCachePages($pages, 'noCheck') ?>
+</div>
+
+<div id="fullPageCacheYes">
+<? showCachePages($pages, 'full') ?>
+</div>
+
+</div>
+
 </form>
 <? return 'Кеш страниц'; } ?>
+<? function showCachePages(&$pages, $thisType){
+?>
+<? foreach($pages as $thisPage => $type){
+	if ($type!= $thisType) continue;
+?>
+<div>
+    <input type="radio" name="fullpageCache[{$thisPage}]" value="" title="Не кешировать" /> - 
+    <input type="radio" name="fullpageCache[{$thisPage}]" value="noCheck" {checked:$type=='noCheck'} title="Кешировать без проверки параметров" />
+    <input type="radio" name="fullpageCache[{$thisPage}]" value="full" {checked:$type=='full'} title="Кешировать с проверкой параметров" />
+ 
+    Кешировать эту страницу <a href="{$thisPage}">{$thisPage}</a>
+</div>
+<? } ?>
+<? } ?>
