d3fb0f450ea3270b35b6aa9b747901e0c78e938a
SnowCat <snowcat@vpro.ru>
2014-11-30 21:06:02 +0500

    ADD notes

--- _modules/_editor/config.install.php
+++ _modules/_editor/config.install.php
@@ -1,26 +1,32 @@
 <?
 //	autoinstall CKEditor from system PHAR ZIP file
 //	Search in zip files _editor folder and extract to system folder
-if (extension_loaded("zip"))
+if (!extension_loaded("zip")) return;
+
+$baseDir= '_editor/';
+//	If folder exists, do not extract any
+if (is_dir($baseDir)) return;
+
+//	Length of folder name for comparsion
+$nLen	= strlen($baseDir);
+//	Open add ZIP files from ROOT folder
+$zip	= new ZipArchive();
+$files	= getFiles('./', 'zip$');
+foreach($files as $file)
 {
-	$baseDir= '_editor/';
-	$nLen	= strlen($baseDir);
-	
-	$zip	= new ZipArchive();
-	$files	= getFiles('./', 'zip$');
-	foreach($files as $file)
+	if (!$zip->open($file)) continue;
+	//	Find files in archive
+	$extractFiles	= array();
+	for($i = 0; $i < $zip->numFiles; $i++)
 	{
-		if (!$zip->open($file)) continue;
-		
-		$extractFiles	= array();
-		for($i = 0; $i < $zip->numFiles; $i++)
-		{
-			$entry = $zip->getNameIndex($i);
-			if (strncmp($entry, $baseDir, $nLen)) continue;
-			$extractFiles[]	= $entry;
-		}
-		$zip->extractTo('./', $extractFiles);
-		$zip->close();
+		$entry = $zip->getNameIndex($i);
+		if (strncmp($entry, $baseDir, $nLen)) continue;
+		//	Append to extract array
+		$extractFiles[]	= $entry;
 	}
+	//	Extract files to ROOT folder
+	if ($extractFiles) $zip->extractTo('./', $extractFiles);
+	//	CLode archive
+	$zip->close();
 }
 ?>
