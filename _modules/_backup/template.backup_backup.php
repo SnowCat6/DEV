de17c3aea9d1c946a8c1ebe99af0507e77dabe63
SnowCat <snowcat@vpro.ru>
2014-12-01 00:31:46 +0500

    UPDATE backup

--- _modules/_backup/template.backup_backup.php
+++ _modules/_backup/template.backup_backup.php
@@ -25,18 +25,18 @@ function backup_backup(&$db, $val, &$data)
 		$time 		= round(getmicrotime() - $timeStart, 4);
 		$note		= "$note\r\nВремя архивирования $time";
 
-		if ($passw) file_put_contents_safe("$backupFolder/password.bin", md5($passw));
-		file_put_contents_safe("$backupFolder/note.txt", $note);
-
-		
 		$freeSpace		= number_format(round(disk_free_space(globalRootPath)/1024/1024), 0);
 		$freeSpace		= "свободно: <b>$freeSpace Мб.</b>";
 		if ($bOK){
+			file_put_contents_safe("$backupFolder/note.txt", $note);
 			if ($passw){
-				$url = getURLEx('', "URL=backup_$backupName.htm");
-				$url2= htmlspecialchars($url);
+				file_put_contents_safe("$backupFolder/password.bin", md5($passw));
+				$url 	= getURLEx('', "URL=backup_$backupName.htm");
+				$url2	= htmlspecialchars($url);
 				module('message', "Архивация завершена \"<b>$backupName</b>\", $freeSpace<br />".
 						"Ссылка для экстренного восстановления <b>$url2</b>");
+						
+				module("backup:makeInstall", $backupName);
 			}else{
 				module('message', "Архивация завершена \"<b>$backupName</b>\", $freeSpace");
 			}
