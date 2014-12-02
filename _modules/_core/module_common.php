70ed633000998cd4ad42ae4e27c6e5183fec2d11
SnowCat <snowcat@vpro.ru>
2014-12-02 18:09:21 +0500

    FIX makeNote

--- _modules/_core/module_common.php
+++ _modules/_core/module_common.php
@@ -43,11 +43,14 @@ function makeNote($val, $nLen = 200)
 	}
 	
 	$minLen	= $nLen - $nLen / 3;
+	if (strlen($val) <= $nLen) return $val;
+	
 	$val	= mb_substr($val, 0, $nLen, 'UTF-8');
 	if (is_int($nPos = mb_strrpos($val, '.')) && $nPos > $minLen)		$val = mb_substr($val, 0, $nPos+1, 'UTF-8');
 	else if (is_int($nPos = mb_strrpos($val, '!')) && $nPos > $minLen)	$val = mb_substr($val, 0, $nPos+1, 'UTF-8');
 	else if (is_int($nPos = mb_strrpos($val, '?')) && $nPos > $minLen)	$val = mb_substr($val, 0, $nPos+1, 'UTF-8');
 	$val .= ' ..';
+	
 	return $val;
 }
 
