9a69b41ee32589f1a975a66d85d56a9c4f72b196
SnowCat <snowcat@vpro.ru>
2015-02-04 13:29:56 +0500

    UPDATE user personal settings

--- _modules/_user/_admin/_property/template.user_property_person.php
+++ _modules/_user/_admin/_property/template.user_property_person.php
@@ -19,26 +19,32 @@ function user_property_person_update(&$data)
 ?>
 <table width="100%" border="0" cellspacing="0" cellpadding="0">
   <tr>
-    <td valign="top">
-<table width="100%" border="0" cellspacing="0" cellpadding="2">
-  <tr>
-    <td nowrap="nowrap">Имя</td>
-    <td width="100%"><input type="text" name="userPerson[name][last_name]" class="input w100" value="{$person[name][last_name]}" /></td>
-  </tr>
-  <tr>
-    <td nowrap="nowrap">Фамилия</td>
-    <td><input type="text" name="userPerson[name][first_name]" class="input w100" value="{$person[name][first_name]}" /></td>
-  </tr>
-  <tr>
-    <td nowrap="nowrap">&nbsp;</td>
-    <td>&nbsp;</td>
-  </tr>
-  <tr>
-    <td nowrap="nowrap">E-Mail</td>
-    <td><input type="text" name="userPerson[email]" value="{$person[email]}" class="input w100" /></td>
-  </tr>
-</table>
-    </td>
+    <td valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="2">
+      <tr>
+        <td nowrap="nowrap">Имя</td>
+        <td width="100%"><input type="text" name="userPerson[name][last_name]" class="input w100" value="{$person[name][last_name]}" /></td>
+      </tr>
+      <tr>
+        <td nowrap="nowrap">Фамилия</td>
+        <td><input type="text" name="userPerson[name][first_name]" class="input w100" value="{$person[name][first_name]}" /></td>
+      </tr>
+      <tr>
+        <td nowrap="nowrap">Должность</td>
+        <td><input type="text" name="userPerson[work]" value="{$person[work]}" class="input w100" /></td>
+      </tr>
+      <tr>
+        <td nowrap="nowrap">&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td nowrap="nowrap">Телефон</td>
+        <td><input type="text" name="userPerson[phone]" value="{$person[phone]}" class="input w100" /></td>
+      </tr>
+      <tr>
+        <td nowrap="nowrap">E-Mail</td>
+        <td><input type="text" name="userPerson[email]" value="{$person[email]}" class="input w100" /></td>
+      </tr>
+    </table></td>
     <td width="150" valign="top" align="center">
 {{user:titleImage:$id=size:250x200;hasAdmin:true}}
     </td>
