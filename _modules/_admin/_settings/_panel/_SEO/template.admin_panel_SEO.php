45a97618a225683a941774bec5b2f3f952d47c66
SnowCat <snowcat@vpro.ru>
2015-03-11 18:05:43 +0500

    UPDATE global SEO

--- _modules/_admin/_settings/_panel/_SEO/template.admin_panel_SEO.php
+++ _modules/_admin/_settings/_panel/_SEO/template.admin_panel_SEO.php
@@ -7,9 +7,11 @@ function admin_SEO(){
 function admin_panel_SEO()
 {
 	if (!access('write', 'admin:SEO')) return;
+	if (testValue('SEO_UPDATE')) module('admin:tabUpdate:site_SEO', array());
 ?>
 
 <form action="{{getURL:admin_SEO}}" method="post" class="admin ajaxForm ajaxReload">
+<input type="hidden" name="SEO_UPDATE" />
 <? module('admin:tab:site_SEO', array())?>
 </form>
 
