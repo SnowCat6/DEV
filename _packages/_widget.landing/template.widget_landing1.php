5a87da18f8056217fe5e26485c52ebf3b9aa96fa
SnowCat <snowcat@vpro.ru>
2015-04-15 20:42:53 +0500

    ADD landingPage test widget

---
+++ _packages/_widget.landing/template.widget_landing1.php
@@ -0,0 +1,18 @@
+<? function widget_landing1($id, $data)
+{
+	$folder	= $data['uploadFolder'];
+?>
+
+<link rel="stylesheet" type="text/css" href="css/widgetLanding.css">
+<div class="widgetLanding1"{!$data[:style]}>
+	<div class="image"{!$data[:imageStyle]}>
+		{{file:image=size:$data[imageSize];uploadFolder:$folder;hasAdmin:top}}
+    </div>
+    
+    <div class="widgetLandingHolder">
+        <div class="widgetLandingTitle">{{read:$id/1}}</div>
+        <div class="widgetLandingContent">{{read:$id/2}}</div>
+    </div>
+</div>
+
+<? } ?>
---
+++ _packages/_widget.landing/template.widget_landingWidgetGenerator.php
@@ -0,0 +1,61 @@
+<? function widget_landingWidgetGenerator_config($val, &$widgets)
+{
+	$widgets[]		=	array(
+		'category'	=> 'Лендинг',
+		'name'		=> 'Фон с информацией',
+		'title'		=> 'Фоновая картинка я заголовком и текстом',
+		'exec'		=> 'widget:landing1:[id]',
+		'update'	=> 'widget:landingUpdate:[id]',
+		'delete'	=> 'widget:landingDelete:[id]',
+		'config'	=> array
+		(
+			'Размер фона (ШxВ)'	=> array(
+				'name'		=> 'data.size',
+				'default'	=> ''
+			),
+			'Цвет фона'	=> array(
+				'name'		=> 'data.style.background',
+				'default'	=> ''
+			)
+		)
+	);
+}
+function widget_landingUpdate($id, &$widget)
+{
+	$property	= array();
+	$propertyImg= array();
+	
+	$size		= $widget['data']['size'];
+	if ($size){
+		list($w, $h) = explode('x', $size);
+		if ($w) $property['width']	= $w . 'px';
+		if ($h) $property['height']	= $h . 'px';
+		if ($h && $w){
+//			$propertyImg['overlay']	= 'hidden';
+//			$propertyImg['width']	= $w . 'px';
+//			$propertyImg['height']	= $h . 'px';
+		}
+	}
+	if (is_array($widget['data']['style'])){
+		foreach($widget['data']['style'] as $name=>$val){
+			$property[$name] = $val;
+		}
+	}
+	
+	$uploadFolder	= images . "/$widget[id]/Title";
+	makeDir($uploadFolder);
+	$widget['data']['uploadFolder']	= $uploadFolder;
+	$widget['imageSize']	= $size;
+
+	
+	$style	= makeStyle($property);
+	$widget['data'][':style'] = " style=\"$style\"";
+
+	$style	= makeStyle($propertyImg);
+	$widget['data'][':imageStyle'] = " style=\"$style\"";
+}
+function widget_landingDelete($id)
+{
+	m("file:unlink", images."/$id");
+}
+?>
