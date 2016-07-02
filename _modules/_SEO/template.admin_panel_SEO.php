4adf6217083800550c0085bd6525a18c1679b15c
SnowCatPDA <snowcat@vpro.ru>
2016-07-02 12:13:04 +0500

    UPDATE SEO tabs to dynamic load

--- _modules/_SEO/template.admin_panel_SEO.php
+++ _modules/_SEO/template.admin_panel_SEO.php
@@ -1,10 +1,19 @@
 <?
-//	+function admin_SEO
-function admin_SEO(){
-	return admin_panel_SEO();
-}
 //	+function admin_panel_SEO
 function admin_panel_SEO()
+{
+	if (!access('write', 'admin:SEO')) return;
+	return array(
+		'name'	=> 'SEO',
+		'URL'	=> getURL('admin_SEO', array(
+			'SEO_DOC' => currentPage()
+			)
+		)
+	);
+};
+
+//	+function admin_SEO
+function admin_SEO()
 {
 	if (!access('write', 'admin:SEO')) return;
 	if (testValue('SEO_UPDATE')) module('admin:tabUpdate:site_SEO', array());
@@ -15,4 +24,4 @@ function admin_panel_SEO()
 <? module('admin:tab:site_SEO', array())?>
 </form>
 
-<? return 'SEO'; } ?>
+<? return ; } ?>
