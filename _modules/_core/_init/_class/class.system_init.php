122c24e79e4ee48d6ad651136c271cdf8e88b518
SnowCatPDA <snowcat@vpro.ru>
2016-08-16 14:01:45 +0500

    OPTIMIZE initialization

--- _modules/_core/_init/_class/class.system_init.php
+++ _modules/_core/_init/_class/class.system_init.php
@@ -45,7 +45,12 @@ class system_init
 		if (!$ini[':']['checkCompileFiles']){
 			setCacheValue('siteFS', NULL);
 		}
-		
+
+		//	 Создать файл .htaccess
+		systemHtaccess::htaccessMake();
+		//	Переместить шаблоны на постоянное место в случае пересборки кода
+		addEvent('config.rebase',	'config_rebase');
+
 		return $bOK;
 	}
 	/******************************************/
@@ -126,6 +131,7 @@ class system_init
 		//	Вычислить время модификации самого свежего файла
 		$maxModifyTime	= 0;
 		$localModules	= array();
+		$localPages		= array();
 
 		$regExpConfig	= '#(^|/)config\.(.*)\.php$#';
 		self::addExcludeRegExp($regExpConfig);
@@ -133,6 +139,8 @@ class system_init
 		self::addExcludeRegExp($regExpModule);
 		$regExpClass	= '#(^|/)class\.([a-zA-Z\d_-]+)\.php#';
 		self::addExcludeRegExp($regExpClass);
+		$regExpTemplates	= '#((page|phone\.page|tablet\.page|template|.*\.template)\.(.*))\.(php|php3)$#';
+		self::addExcludeRegExp($regExpTemplates);
 		
 		foreach($siteFS as $vpath => $path)
 		{
@@ -148,15 +156,19 @@ class system_init
 			}else
 			//	Collect all possibly classes inside class. files
 			if (preg_match($regExpClass, $vpath, $val)){
-				//	Remove class path form possibly check change files (classes not copy to cache)
-				unset($siteFS[$vpath]);
-				
 				$class			= $val[2];
 				$classes[$class]= $path[0];
 				$content		= file_get_contents($path[0]);
 				self::scanCotentForClass($classes, $content, $path[0]);
+				//	Remove class path form possibly check change files (classes not copy to cache)
+				unset($siteFS[$vpath]);
+			}else
+			//	Collect pages and templates
+			if (preg_match($regExpTemplates, $vpath, $val)){
+				$name				= $val[1];
+				$localPages[$name]	= $path[0];
 			}
-		};
+		}
 		setCacheValue('siteFS', $siteFS);
 
 		//	Сохранить список классов
@@ -182,6 +194,8 @@ class system_init
 		module('message:trace:modules', trim(ob_get_clean()));
 		$time 		= round(getmicrotime() - $timeStart, 4);
 		m("message:trace", "$time Included $compiledPath file");
+		
+		self::pageInitializeCompile($cacheRoot, $localPages); 
 	}
 
 	//	Пройти по списку моулей и объеденить в один файл и оптимизировать для выполнения
@@ -208,6 +222,198 @@ class system_init
 		
 		return $modules;
 	}
+	//	Найти функции с названием модулей и добавть в список
+	static function findAndAddModules(&$templates, $src, $filePath)
+	{
+		//	Modules and functions
+		if (preg_match_all('#//\s+\+\s*function\s+([\w\d_]+)#', $src, $val))
+		{
+			foreach($val[1] as $m){
+				$templates[$m]	= $filePath;
+			}
+		}
+	
+		//	classes
+		$classes	= getCacheValue(":classes");
+		if (self::scanCotentForClass($classes, $src, $filePath)){
+			setCacheValue(":classes", $classes);
+		}
+	}
+	//	Compile pages
+	//	Обработать список файлов, скомилировать и скопировать в кеш
+	static function pageInitializeCompile($cacheRoot, $localPages)
+	{
+		$pages				= array();	//	Страницы
+		$templates			= array();	//	Шаблоны
+		
+		$comiledFileTime	= NULL;
+		$comiledTemplates	= array();	//	Шаблоны для компиляции в один файл
+		$compiledTmpName	= "$cacheRoot/".localCompilePath."/compiled.php3";
+	
+		//	Пройти по всему списку файлов
+		foreach($localPages as $name => &$pagePath)
+		{
+			$fileName	= basename($pagePath);
+			//	Файлы с расширением php3 объеденяются в один файл
+			if (preg_match('#^(template)\.(.*)\.php3$#', $name, $v))
+			{
+				$name					= $v[2];
+				$comiledFileTime		= max($comiledFileTime, filemtime($pagePath));
+				
+				$templates[$name]		= $compiledTmpName;
+				$comiledTemplates[$name]= $pagePath;
+				//	Заменить путь в массиве на общий для всех файлов
+				$pagePath 				= $compiledTmpName;
+				continue;
+			}
+			
+			$compiledPagePath	= "$cacheRoot/".localCompilePath."/$fileName";
+			//	Сравнить файл и файл в кеше, если даты модификации различаются, перекомпилировать
+			if (filemtime($pagePath) != filemtime($compiledPagePath))
+			{
+				//	Прочитать содержимое
+				$compiledPage	= file_get_contents($pagePath);
+				//	Компиляция
+				$ev	= array('source' => $pagePath, 'content' => &$compiledPage);
+				event('page.compile', $ev);
+				//	Найти функции с названием модулей
+				self::findAndAddModules($templates, $compiledPage, $compiledPagePath);
+				//	Сохранить в кеше
+				if (!file_put_contents_safe($compiledPagePath, $compiledPage))
+					return false;
+				//	Присвоить время изменения аналогичное исходному файлу
+				touch($compiledPagePath, filemtime($pagePath));
+			}else{
+				//	Прочитать содержимое
+				$compiledPage	= file_get_contents($compiledPagePath);
+				//	Найти функции с названием модулей
+				self::findAndAddModules($templates, $compiledPage, $compiledPagePath);
+			}
+			//	Сохранить название модуля
+			if (preg_match('#^(template|.*\.template)\.#', $name))
+			{
+				$name	= preg_replace('#^template\.#', '', 			$name);
+				$name	= preg_replace('#^(.*)\.template\.#', '\\1_',	$name);
+				
+				$templates[$name]		= $compiledPagePath;
+			}else
+			//	Сохранить название страницы
+			if (preg_match('#^(page|.*\.page)\.#', $name)){
+				$pages[$name]		= $compiledPagePath;
+			}
+		}
+		
+		//	Собрать файлы в единый компилированный файл
+		if ($comiledFileTime > filemtime($compiledTmpName))
+		{
+			$compiledTemplate	= '';
+			//	Прочитать все файлы и объеденить в один
+			foreach($comiledTemplates as $name => &$pagePath)
+			{
+				$compiledPage		= file_get_contents($pagePath);
+				//	Компилировать файл
+				$ev	= array('source' => $pagePath, 'content' => &$compiledPage);
+				event('page.compile', $ev);
+				
+				$compiledTemplate	.=$compiledPage;
+			}
+			//	Найти функции с названием модулей
+			self::findAndAddModules($templates, $compiledPage, $compiledTmpName);
+			//	Сохранить файл
+			file_put_contents_safe($compiledTmpName, $compiledTemplate);
+		}else{
+			$compiledTemplate	= file_get_contents($compiledTmpName);
+			//	Найти функции с названием модулей
+			self::findAndAddModules($templates, $compiledTemplate, $compiledTmpName);
+		}
+		
+		//	Сохранить названия модулей
+		setCacheValue('templates',		$templates);
+		//	Сохранить названия страниц
+		setCacheValue('pages', 			$pages);
+		
+		return true;
+	}
 
+}
+
+//	Переместить все ссылки на исполняемые функции на новое место
+//	Вызывается после пересобрания всей системы во временном месте
+function module_config_rebase($val, $thisPath)
+{
+	fnMoveSysFiles('templates', $thisPath);
+	fnMoveSysFiles('pages', 	$thisPath);
+	fnMoveSysFiles(':classes', 	$thisPath);
+}
+function fnMoveSysFiles($cacheName, $thisPath)
+{
+	$nLen	= strlen($thisPath);
+	$files	= getCacheValue($cacheName);
+	foreach($files as &$path){
+		if (strncmp($path, $thisPath, $nLen)) continue;
+		$path	= cacheRoot . substr($path, $nLen);
+	}
+	setCacheValue($cacheName, $files);
+}
+?>
+
+<?
+//	FIRST executed config
+//	Define one time used functions
+$ini	= getIniValue(':');
+if (!is_array($ini)){
+	$ini['useCache']			= 1;
+	$ini['compress']			= 'gzip';
+	$ini['checkCompileFiles']	= 1;
+	setIniValue(':', $ini);
+}
+
+//	Добавть обработчик события
+function addEvent($eventName, $eventModule)
+{
+	$event = getCacheValue('localEvent');
+
+	//	Можно задавать место выполнения события
+	//	addEvent('config.end:before', ...);
+	list($eventName, $postfix)		= explode(':', $eventName, 2);
+	if (!$postfix) $postfix = 'fire';
+	//	Добавить событие
+	$event[$eventName][$postfix][$eventModule]	= $eventModule;
+
+	setCacheValue('localEvent', $event);
+}
+
+//	Добавить обработчки URL страницы
+function addUrl($parseRule, $parseModule){
+	addUrlEx("#^/$parseRule\.htm$#i", $parseModule);
+}
+//	Добавить обработчки URL страницы
+function addUrlEx($parseRule, $parseModule)
+{
+	$localURLparse = getCacheValue('localURLparse');
+	$localURLparse[$parseRule]	= $parseModule;
+	setCacheValue('localURLparse', $localURLparse);
+}
+
+//	Добавить обработку правила доступа к объектам
+function addAccess($parseRule, $parseModule){
+	$localAccessParse = getCacheValue('localAccessParse');
+	$localAccessParse[$parseRule][$parseModule]	= $parseModule;
+	setCacheValue('localAccessParse', $localAccessParse);
+}
+
+//	Добавить групповую роль для администрирования
+function addRole($roleName, $roleAccess){
+	$localUserRoles = getCacheValue('localUserRoles');
+	$localUserRoles[$roleAccess]	= $roleName;
+	setCacheValue('localUserRoles', $localUserRoles);
+}
+
+//	Добавить фиксированный сниппет
+function addSnippet($snippetName, $value){
+	$localSnippets = getCacheValue('localSnippets');
+	$localSnippets[$snippetName]	= $value;
+	if (!$value) unset($localSnippets[$snippetName]);
+	setCacheValue('localSnippets', $localSnippets);
 }
 ?>
