ba2d3da60232c4ec2e0b1320ad29ef8462da2bb1
SnowCatPDA <snowcat@vpro.ru>
2017-01-12 22:44:24 +0500

    ADD codemirror - disabled
    Not work in new overlay windows

--- _modules/_holder/_widgets/module_widget.php
+++ _modules/_holder/_widgets/module_widget.php
@@ -1,36 +1,41 @@
 <?
 function module_widget($fn, $data)
 {
-	list($widgetClass, $id) = explode(':', $fn, 2);
-	
+	list($widgetClass, $widgetID) = explode(':', $fn, 2);
+	if (!$widgetID) return;
+
 	$widgets	= getCacheValue(':holderWidgets');
 	//	Обновить кеш виджетов
 	if (!$widgets)
 	{
 		$widgets= getStorage("holder/widgets", 'ini') or array();
-		$widget	= $widgets[$id];
 		foreach($widgets as &$w){
 			$w	= module("holderAdmin:widgetPrepare", $w);
 		}
 		setCacheValue(':holderWidgets', $widgets);
 	}
 
-	$widget	= $widgets[$id];
+	$widget	= $widgets[$widgetID];
 	if (!$widget)
 	{
 		$widget	= widgetHolder::findWidget($widgetClass, NULL);
 		if (!$widget) return;
 
 		$widget	= module("holderAdmin:widgetPrepare", $widget);
-		$widgets[$id] = $widget;
+		$widgets[$widgetID] = $widget;
 		setCacheValue(':holderWidgets', $widgets);
 	}
-	
-	$fn	= getFn("widget_$widget[className]");
-	if (!$fn || !$id) return;
 
 	$exec	= $widget[':exec'];
-	if (!$exec['code'] || $widget['hide']) return;
-	return $fn($id, $exec['data']);
+	if ($widget['hide']) return;
+
+	list($m, $p, $v) = explode(':', $exec['code'], 3);
+	//	Skip re-call module widget
+	if ($m != "widget") return module($exec['code'], $exec['data']);	
+
+	$fn	= getFn("widget_$p");
+	if (!$fn) return;
+	
+	return $fn($v, $exec['data']);
 }
 ?>
