f6adacaed77e9109128ec6094b891d261c5378bb
SnowCatPDA <snowcat@vpro.ru>
2017-09-05 23:43:55 +0500

    UPDATE document gallery admin

--- _modules/_gallery/_gallery.uploadFull/template.doc_property_uploadFull.php
+++ _modules/_gallery/_gallery.uploadFull/template.doc_property_uploadFull.php
@@ -2,31 +2,33 @@
 {
 	m('script:jq_ui');
 	m('script:fileUpload');
+	
+	$folders	= array(
+		'gallery:upload:Title'		=> 'Обложка документа',
+		'gallery:uploadFull:Image'	=> 'Фотогаллерея',
+		'gallery:uploadFull:Gallery'=> 'Изображения в документе',
+		'gallery:uploadFull:File'	=> 'Файлы документа'
+	);
+	$ev	= array(
+		'data'		=> $data,
+		'folders'	=> &$folders
+	);
+	event('document.property_uploadFull', $ev);
 ?>
 <link rel="stylesheet" type="text/css" href="../../../_templates/baseStyle.css" />
 <div class="adminTabs ui-tabs ui-widget ui-widget-content ui-corner-all">
-<ul class="ui-tabs-nav ui-helper-reset ui-helper-clearfix ui-widget-header ui-corner-all">
-    <li class="ui-corner-top"><a href="#uploadTitle">Обложка документа</a></li>
-    <li class="ui-corner-top"><a href="#uploadGallery">Фотогаллерея</a></li>
-    <li class="ui-corner-top"><a href="#uploadImage">Изображения в документе</a></li>
-    <li class="ui-corner-top"><a href="#uploadFile">Файлы документа</a></li>
-</ul>
+    <ul class="ui-tabs-nav ui-helper-reset ui-helper-clearfix ui-widget-header ui-corner-all">
+<? foreach($folders as $module=>$name){ ?>
+        <li class="ui-corner-top"><a href="#{$module}">{$name}</a></li>
+<? } ?>
+    </ul>
 
-<div id="uploadTitle" class="ui-tabs-panel ui-widget-content ui-corner-bottom">
-<? module("gallery:upload:Title", $data) ?>
-</div>
+<? foreach($folders as $module=>$name){ ?>
+    <div id="{$module}" class="ui-tabs-panel ui-widget-content ui-corner-bottom">
+    <? module($module, $data) ?>
+    </div>
+<? } ?>
 
-<div id="uploadImage" class="ui-tabs-panel ui-widget-content ui-corner-bottom">
-<? module('gallery:uploadFull:Image', $data) ?>
-</div>
-
-<div id="uploadGallery" class="ui-tabs-panel ui-widget-content ui-corner-bottom">
-<? module('gallery:uploadFull:Gallery', $data) ?>
-</div>
-
-<div id="uploadFile" class="ui-tabs-panel ui-widget-content ui-corner-bottom">
-<? module('gallery:uploadFull:File', $data) ?>
-</div>
 </div>
 
 {{script:adminTabs}}
